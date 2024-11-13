dab2d9c2819fcd00db77eb589fc33ffe6edd512c
helelys <tallyslabanca20@gmail.com>
2024-11-13 10:01:41 -0300

    about content

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderBarComponent } from "../../components/header-bar/header-bar.component";
 import { HeroBannerComponent } from "../../components/hero-banner/hero-banner.component";
+import { AboutContentComponent } from "../../components/about-content/about-content.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [HeaderBarComponent, HeroBannerComponent],
+  imports: [HeaderBarComponent, HeroBannerComponent, AboutContentComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
